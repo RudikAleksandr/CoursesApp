857fb394c22f64bc7460d97619f6c7ae05e6aeb1
Aliaksandr Rudzin <Aliaksandr_Rudzin@epam.com>
2020-09-15 21:31:37 +0300

    Add Breadcrumbs component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import { Header } from './components/header';
 import { Footer } from './components/footer';
+import { Breadcrumbs } from './components/breadcrumbs';
 
 function App() {
   return (
     <>
       <Header userFirstName='Aleksandr' 
         userLastName='Rudin' />
+      <Breadcrumbs />
       <Footer />
     </>
   );
---
+++ src/components/breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Breadcrumbs.scss';
+
+function Breadcrumbs(props: any) {
+  return (
+    <nav className="breadcrumbs breadcrumbs-height">
+      <ol className="breadcrumb breadcrumbs__item content-padding">
+        <li className="breadcrumb__item">
+          <a href="" className="breadcrumbs__link">Courses</a>
+        </li>
+        <li className="breadcrumb__item">
+            <span className="breadcrumbs__link">Courses/</span>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+export default Breadcrumbs;
